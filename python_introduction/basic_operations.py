a94ff7d48438aecab220ed35e8e440e53f93579f
OfficialMNM <methusellanyongesa004@gmail.com>
2025-07-08 09:15:59 +0300

    python script that performs basic arithmetic operations: addition, subtraction, and multiplication

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,7 +1,7 @@
 # This script performs basic arithmetic operations: addition, subtraction, and multiplication.
 
-number1 = 10  # First number 
-number2 = 5 # Second number
+number1 = 10  # variable for the first number 
+number2 = 5 # variable for the second number
 
 # Addition
 result1 = number1 + number2
@@ -10,6 +10,7 @@ result2 = number1 - number2
 # Multiplication
 result3 = number1 * number2
 
+# Displaying the results
 print("Addition of", number1, "and", number1, "is", result1)
 print("Subtraction of", number1, "and", number1, "is", result2)
 print("Multiplication of", number1, "and", number1, "is", result3)
