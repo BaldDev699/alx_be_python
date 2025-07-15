332ff0c886619a8788037f9eab74aba123f5a2e1
OfficialMNM <methusellanyongesa004@gmail.com>
2025-07-15 06:44:20 +0300

    python script that generates a multiplication table for a given number

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -4,4 +4,4 @@ number = int(input("Enter a number to see its multiplication table: ")) # Input 
 
 for i in range(1, 11):  # Loop from 1 to 10
     number * i  # Calculate the product
-    print(f"{number} x {i} = {number * i}")  # Print the multiplication result
+    print(f"{number} * {i} = {number * i}")  # Print the multiplication result
