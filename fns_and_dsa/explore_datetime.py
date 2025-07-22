1bd058d96bce3c7c71cc0fabdea17d22d834b49f
OfficialMNM <methusellanyongesa004@gmail.com>
2025-07-22 06:43:12 +0300

    python script that explores datetime functionalities in Python

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -14,7 +14,7 @@ def calculate_future_date(days):
     future_date = today + timedelta(days=days)
     print("Future date:", future_date.strftime("%Y-%m-%d")) # displaying future date based on user input
 try:
-    user_input = int(input("Enter the number of days to add to today's date: ")) # taking user input
+    user_input = int(input("Enter the number of days to add to the current date: ")) # taking user input
     calculate_future_date(user_input) # calculating future date based on user input
 except ValueError:
     print("Invalid input. Please enter a valid number.") # handling invalid input
