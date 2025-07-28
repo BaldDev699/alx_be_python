5d752d69323f2721cd5ad57437cd814ead2a03bb
BaldDev699 <methusellanyongesa057@gmail.com>
2025-07-28 15:37:08 +0300

    python script that tests the SimpleCalculator class

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -22,7 +22,7 @@ class TestSimpleCalculator(unittest.TestCase): # Creating a test case class for 
         self.assertEqual(self.calc.subtract(0, 0), 0)
         self.assertEqual(self.calc.subtract(-1, -1), 0)
 
-    def test_multiply(self):
+    def test_multiplication(self):
         """Test the multiplication method."""
         self.assertEqual(self.calc.multiply(2, 3), 6)
         self.assertEqual(self.calc.multiply(-1, 1), -1)
