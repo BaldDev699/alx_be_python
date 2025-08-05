29eef17a4ef182d419fbd8c55c28bbd2d1711ab7
Aiko543 <njokistephanie01@gmail.com>
2025-08-05 09:02:48 +0300

    python sript to manage a library system with different types of books

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,22 +1,31 @@
-# Sript to manage a library system with different types of books
+# Script to manage a library system with different types of books
 
 # Class for a generic Book
 class Book:
     def __init__(self, title, author):
         self.title = title
         self.author = author
+    
+    def __str__(self):
+        return f"Book: {self.title} by {self.author}"
 
 # Class for EBook inheriting from Book
 class EBook(Book):
     def __init__(self, title, author, file_size):
         super().__init__(title, author)
         self.file_size = file_size
+    
+    def __str__(self):
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}MB"
 
 # Class for PrintBook inheriting from Book
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
         super().__init__(title, author)
         self.page_count = page_count
+    
+    def __str__(self):
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
 # Class to manage a collection of books
 class Library:
@@ -28,9 +37,4 @@ class Library:
 
     def list_books(self):
         for book in self.books:
-            if isinstance(book, EBook):
-                print(f"E-Book: {book.title} by {book.author}, File Size: {book.file_size}MB")
-            elif isinstance(book, PrintBook):
-                print(f"Print Book: {book.title} by {book.author}, Page Count: {book.page_count}")
-            else:
-                print(f"Book: {book.title} by {book.author}")
+            print(book)
